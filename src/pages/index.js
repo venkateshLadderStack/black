c10ef8e8fd12a8131cc9c12d3d2b45fff84b553c
venkaCivsav <venka.civilizedsavage@gmail.com>
2022-01-18 19:20:01 +0530

    chnages

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,17 +13,8 @@ const IndexPage = () => {
           height: "100vh",
           backgroundPosition: "center center",
           backgroundRepeat: "no-repeat",
-          backgroundSize: "contain",
-        }}
-      />
-      <div
-        style={{
-          backgroundImage: `url(${home?.splashBg2})`,
-          width: "100vw",
-          height: "100vh",
-          backgroundPosition: "center center",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "contain",
+          backgroundSize: "cover",
+          backgroundPositionY: "inherit",
         }}
       />
     </div>
