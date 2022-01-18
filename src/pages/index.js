aa6516f6df0f570ea408672242f8284361535c27
venkaCivsav <venka.civilizedsavage@gmail.com>
2022-01-18 14:18:12 +0530

    added splash screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+
 import "../styles/globalStyles.css"
 import home from "../../site/home/home.json"
 import Hero from "../components/Home/Hero"
@@ -7,18 +8,21 @@ import Section2 from "../components/Home/Section2"
 import Section3 from "../components/Home/Section3"
 import Section4 from "../components/Home/Section4"
 import Section5 from "../components/Home/Section5"
+import Layout from "../components/layout"
 
 const IndexPage = () => {
   return (
-    <div className="wrapper">
-      <Hero heroImage={home?.heroImage} />
-      <Section1 data={home?.sectionOne} />
-      <Section2 data={home?.sectionTwo} />
-      <Section3 data={home?.sectionThree} />
-      <Section4 data={home?.sectionFour} />
-      <Section5 data={home?.sectionFive} />
-      <Hero heroImage={home?.footerHeroImage} isHero={false} />
-    </div>
+    <Layout splash={home?.splashBg1}>
+      <div className="wrapper">
+        <Hero heroImage={home?.heroImage} />
+        <Section1 data={home?.sectionOne} />
+        <Section2 data={home?.sectionTwo} />
+        <Section3 data={home?.sectionThree} />
+        <Section4 data={home?.sectionFour} />
+        <Section5 data={home?.sectionFive} />
+        <Hero heroImage={home?.footerHeroImage} isHero={false} />
+      </div>
+    </Layout>
   )
 }
 
