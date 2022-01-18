a35996037799cd8d2bbbf2fe47b626ce3bb2f5b2
venkaCivsav <venka.civilizedsavage@gmail.com>
2022-01-18 16:14:59 +0530

    click to scroll

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,7 +1,9 @@
 import React from "react"
 import ArrowDown from "../../images/Vector.svg"
+import { useScrollBy } from "react-use-window-scroll"
 
 const Hero = ({ heroImage, isHero = true }) => {
+  const scrollBy = useScrollBy()
   return (
     <section style={{ overflow: "hidden" }}>
       <div className="row">
@@ -9,13 +11,22 @@ const Hero = ({ heroImage, isHero = true }) => {
           style={{
             backgroundImage: `url(${heroImage})`,
             width: "100%",
-            height: "100%",
+            height: isHero ? "80%" : "100%",
             backgroundPosition: "center center",
             backgroundRepeat: "no-repeat",
           }}
         />
         <div className="col-12 hero_img_wrapper">
-          {isHero && <img src={ArrowDown} alt="" className="arr_dwn" />}
+          {isHero && (
+            <img
+              src={ArrowDown}
+              onClick={() =>
+                scrollBy({ top: 710, left: 0, behavior: "smooth" })
+              }
+              alt=""
+              className="arr_dwn"
+            />
+          )}
         </div>
       </div>
     </section>
