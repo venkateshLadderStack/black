ca599c600b5fd252cb154a17a926a85a099d60b5
karthikeyan Seenivasan <karthikeyan@ladderstack.com>
2022-02-11 19:17:05 +0530

    contact page added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,11 +3,49 @@ import bannerImg from "../images/banner_img.png"
 import sign_img from "../images/signature.png"
 import Button from "./Button"
 import * as banner from "../styles/banner.module.css"
-
+import video from "../images/homepage_video.mp4"
+import "../../node_modules/video-react/dist/video-react.css"
+import { ControlBar, Player } from "video-react"
 const Banner = () => {
   return (
     <>
-      <div
+      <div className={banner.vedioArea}>
+        <Player
+          poster={bannerImg}
+          fluid={true}
+          muted
+          autoPlay={true}
+          src={video}
+          loop
+        >
+          <ControlBar disableCompletely autoHide={true} />
+        </Player>
+      </div>
+      <div className={banner.contentArea}>
+        <div className={banner.content}>
+          <div className="ms-5">
+            <img src={sign_img} width={330} height={163} alt="" />
+          </div>
+          <div className={banner.button}>
+            <div className="me-3">
+              <Button name="Learn more" />
+            </div>
+            <Button classN="green" name="Where to Find Us" />
+          </div>
+        </div>
+      </div>
+      {/* <div
+        style={{
+          position: "relative",
+          backgroundImage: `url(${bannerImg})`,
+          width: "auto",
+          height: "100vh",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "top",
+        }}
+      ></div> */}
+      {/* <div
         style={{
           position: "relative",
           backgroundImage: `url(${bannerImg})`,
@@ -31,7 +69,20 @@ const Banner = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
+      {/* <video className={banner.bg_video} muted={false} autoPlay>
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className={banner.bg_video}>
+        <iframe
+          src={video}
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          frameBorder="0"
+          webkitallowfullscreen="true"
+          mozallowfullscreen="true"
+          allowFullScreen
+        />
+      </div> */}
     </>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,20 +3,27 @@ import Layout from "../components/Layout/Layout"
 import Seo from "../components/seo"
 import Signature from "../components/Signature"
 import * as globalStyle from "../styles/global.module.css"
+import sign_img from "../images/sign_dark.png"
+import bottle_img from "../images/bottle2.png"
+import * as contactStyle from "../styles/contact.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import * as poster from "../styles/poster.module.css"
+
+import {
+  faFacebookF,
+  faInstagram,
+  faTiktok,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons"
+import InstaFeed from "../components/InstaFeed"
+import useWindowSize from "../../hooks/useWindowSize"
 
 function Contact() {
-  const [startAnim, setStartAnim] = useState(false)
-  const loadAnim = () =>
-    setTimeout(() => {
-      setStartAnim(true)
-    }, 2500)
-
-  useEffect(() => {
-    loadAnim()
-  }, [])
+  const { width } = useWindowSize()
 
   return (
     <>
+      {width}
       <Seo title="Contact" description="Black Irish" />
       <Layout>
         <div className={globalStyle.page}>
@@ -24,9 +31,82 @@ function Contact() {
             <h1>Contact</h1>
           </div>
           <div className={globalStyle.border}></div>
-          <div className="bg-dark">
-            {startAnim && <Signature startAnim={startAnim} />}
+        </div>
+        <div className="container my-5">
+          <div className="row d-flex flex-row-reverse">
+            <div className="col-lg-6 col-md-12 col-sm-12 col-12 d-flex justify-content-center">
+              <img
+                width={width < 500 ? 249.2 : width < 700 ? 358 : 494.51}
+                height={width < 500 ? 280 : width < 700 ? 390 : 550}
+                src={bottle_img}
+                alt="bottle_img"
+              />
+            </div>
+            <div className="col-lg-6 col-md-12 col-sm-12 col-12 px-1">
+              <img
+                height={width < 500 ? 131.6 : 158.83}
+                width={width < 500 ? 280 : 334.19}
+                src={sign_img}
+                alt="sign_img"
+              />
+              <div className="row my-1 mx-1 ">
+                <div
+                  className={`col-lg-6 col-md-12 col-sm-12 col-12 ${contactStyle.input_area}`}
+                >
+                  <label htmlFor="name">Name</label>
+                  <input type="text" />
+                </div>
+                <div
+                  className={`col-lg-6 col-md-12 col-sm-12 col-12 ${contactStyle.input_area}`}
+                >
+                  <label htmlFor="name">Email</label>
+                  <input type="text" />
+                </div>
+                <div className={`my-4  ${contactStyle.input_area}`}>
+                  <label htmlFor="name">Message</label>
+                  <textarea type="text" />
+                </div>
+                <div className="row d-flex flex-row justify-content-between my-3 ">
+                  <div className="mb-3 col-lg-6 col-md-12 col-sm-12 col-12 d-flex justify-content-center">
+                    <a
+                      target="_blank"
+                      className="text-dark social_icon mx-2"
+                      href="https://www.facebook.com/goblackirish/"
+                    >
+                      <FontAwesomeIcon icon={faFacebookF} size="2x" />
+                    </a>
+                    <a
+                      target="_blank"
+                      className="text-dark social_icon mx-2"
+                      href="https://www.instagram.com/goblackirish/"
+                    >
+                      <FontAwesomeIcon icon={faInstagram} size="2x" />
+                    </a>
+                    <a
+                      target="_blank"
+                      className="text-dark social_icon mx-2"
+                      href="https://twitter.com/goblackirish?lang=en"
+                    >
+                      <FontAwesomeIcon icon={faTwitter} size="2x" />
+                    </a>
+                    <a
+                      target="_blank"
+                      className="text-dark social_icon mx-2"
+                      href="https://www.tiktok.com/@goblackirish"
+                    >
+                      <FontAwesomeIcon icon={faTiktok} size="2x" />
+                    </a>
+                  </div>
+                  <div className="col-lg-6 col-md-12 col-sm-12 col-12  d-flex justify-content-center">
+                    <button className={` ${poster.green}`}>Send</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
+        <div className="my-5">
+          <InstaFeed />
         </div>
       </Layout>
     </>
